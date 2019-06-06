44b066b5792c34c29298a4c05b19b32a65ee7659
soju6jan <cybersol@naver.com>
2019-06-06 16:19:01 +0900

    1

---
+++ docker_start2.sh
@@ -0,0 +1,31 @@
+#! /bin/sh
+COUNT=0
+while [ 1 ];
+do
+    git reset --hard HEAD
+    git pull
+    FILENAME="update_requirements.txt"
+    if [ -f "$FILENAME" ] ; then
+        pip install -r update_requirements.txt
+    fi
+    export FLASK_APP=sjva.py
+    if [ ! -d "/app/migrations" ] && [ -f "/app/data/db/site.db" ]; then
+        python -OO -m flask db init
+    fi
+    if [ -d "/app/migrations" ]; then
+        python -OO -m flask db migrate
+        python -OO -m flask db upgrade
+    fi
+    chmod +x /app/bin/Linux/rclone
+    chmod +x /app/bin/Linux/chromedriver
+    python -OO sjva.py 9999 ${COUNT}
+    RESULT=$?
+    echo "PYTHON EXIT CODE : ${RESULT}.............."
+    if [ "$RESULT" = "1" ] || [ "$RESULT" = "2" ]; then
+        echo 'REPEAT....'
+    else
+        echo 'FINISH....'
+        break
+    fi
+    COUNT=`expr $COUNT + 1`
+done
